fbd682b06c75802e9952e4b7e0e224958011404d
cristinarosa97 <cristinarosaquero@gmail.com>
2025-01-17 11:56:25 +0100

    w4 lab1 done

--- create.sql
+++ create.sql
@@ -2,11 +2,45 @@ CREATE DATABASE IF NOT EXISTS lab_mysql;
 
 USE lab_mysql;
 
-DROP TABLE IF EXISTS cars;
-CREATE table cars (VIN VARCHAR(25), manufacturer VARCHAR(50), model VARCHAR(50), year INT, color VARCHAR(20));
-DROP TABLE IF EXISTS customers;
-CREATE table customers (customer_id VARCHAR(25), name VARCHAR(30), email VARCHAR(50), phone_number INT, address VARCHAR(50), city VARCHAR(30), country VARCHAR(50), postal_code INT);
-DROP TABLE IF EXISTS salespersons;
-CREATE table salespersons (staff_id VARCHAR(25), name VARCHAR(50), store_id VARCHAR(50));
 DROP TABLE IF EXISTS invoices;
-CREATE table invoices (invoice_number INT, car VARCHAR(25), date DATETIME, customer_id VARCHAR(25), staff_id VARCHAR(25));
+DROP TABLE IF EXISTS cars;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS salespersons;
+
+CREATE TABLE cars (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    VIN VARCHAR(30) NOT NULL,
+    manufacturer VARCHAR(50) NOT NULL,
+    model VARCHAR(50) NOT NULL,
+    year INT NOT NULL,
+    color VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE invoices (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    invoice_number INT NOT NULL,
+    car_id INT UNSIGNED NOT NULL,
+    date DATETIME NOT NULL,
+    customer_id VARCHAR(25) NOT NULL,
+    staff_id INT NOT NULL,
+    FOREIGN KEY (car_id) REFERENCES cars(id)
+);
+
+CREATE TABLE customers (
+    customer_id VARCHAR(25) NOT NULL AUTO_INCREMENT PRIMARY KEY,  -- Remove UNSIGNED
+    name VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    phone_number INT NOT NULL,
+    address VARCHAR(50) NOT NULL, 
+    city VARCHAR(30) NOT NULL, 
+    country VARCHAR(50) NOT NULL,
+    postal_code INT NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES invoices(customer_id)  -- Ensure types match in invoices
+);
+
+CREATE TABLE salespersons (
+    staff_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    store VARCHAR(50) NOT NULL,
+    FOREIGN KEY (staff_id) REFERENCES invoices(staff_id)  -- Ensure types match in invoices
+);
--- seeding.sql
+++ seeding.sql
@@ -18,7 +18,7 @@ VALUES (10001, "Pablo Picasso", "-", 34636176382, "Paseo de la Chopera, 14",	"Ma
 (20001,	"Abraham Lincoln","-",	13059077086, 	"120 SW 8th St",	"Miami", "United States",	33130),
 (30001, "Napoléon Bonaparte","-",	33179754000, "40 Rue du Colisée",	"Paris", "France",	75008);
 
-INSERT INTO invoices (invoice_number, date, car, customer_id, staff_id)
+INSERT INTO invoices (invoice_number, date, car_id, customer_id, staff_id)
 VALUES (852399038,	'2018-08-22',	1,	1,	3),
 		(731166526,	'2018-12-31',	3,	3,	5),
         (271135104,	'2019-01-22',	2,	2,	7);
